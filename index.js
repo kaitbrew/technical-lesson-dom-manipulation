c1d99ada186ad25142b0f5471eec92d0c354c70b
kait brewer <kaitlynmbrewer@gmail.com>
2025-06-02 21:10:37 -0400

    DOM practice

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-// Write your code here!
+const userInfo = {
+  username: "flatUser123",
+  description: "hi, welcome to my webpage. just a new user of this website!",
+  theme: "blue",
+};
 
+function displayUser(userInfo){
+const header=document.querySelector("#main")
+header.textContent=userInfo.username+"'s Personal Webpage"
+header.className=userInfo.theme
+
+const description=document.createElement("p")
+const body=document.querySelector("body")
+
+description.textContent=userInfo.description
+description.className=userInfo.theme
+
+body.append(description)
+}
+
+displayUser(userInfo)
